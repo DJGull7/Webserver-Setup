e2eab49c5c4682b694dd61e3517786093e5e265a
DJGull7 <dj.gull.7@gmail.com>
2020-11-18 19:18:31 -0500

    Update1

--- Html-testing/list.js
+++ Html-testing/list.js
@@ -1,1 +1,13 @@
 //for storing arrays and what not
+class { waifu ({
+  this:name = name,
+  this:desc = desc
+});
+}
+let rem = new waifu ({
+  name = "rem",
+  desc  = "REEEEEEEEEEEEEEEEEEEEEEEEEM"
+});
+
+console.log(rem)
+
--- Node-Testing/array.js
+++ Node-Testing/array.js
@@ -4,3 +4,4 @@ var passwords = ["password123", "PassWord1234"];
 console.log(passwords);
 
 
+var input = ["why"]
